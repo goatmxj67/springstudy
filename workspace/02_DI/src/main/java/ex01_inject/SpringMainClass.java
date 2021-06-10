06c105c755bda34512d32ff2887f23ba9d75f73b
goatmxj67 <yulki67@naver.com>
2021-06-10 17:48:25 +0900

    update 2021-06-10

---
+++ workspace/02_DI/src/main/java/ex01_inject/SpringMainClass.java
@@ -0,0 +1,12 @@
+package ex01_inject;
+
+public class SpringMainClass {
+
+	public static void main(String[] args) {
+		
+		SelectListCommand selectListCommand = new SelectListCommand();
+		selectListCommand.execute();
+
+	}
+
+}
